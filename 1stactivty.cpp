5fdd89879c377a641a4c79662645fccef2117c8f
wesleythegreat <gameking232nd@gmail.com>
2022-09-17 18:10:34 +0800

    first activity handson in forpr

---
+++ 1stactivty.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int num1, num2, choice;
+	cout << "enter two numbers ";
+	cin >> num1 >> num2;
+	cout <<"select a number for a type of operator will be used" << endl;
+	cout <<"1. addition"<<endl;
+	cout <<"2. subtraction"<<endl;
+	cout <<"3. division"<<endl;
+
+	switch(choice){
+	case 1:
+		cout << num1<<"+"<<num2<<"="<<num1+num2;
+		break;
+	case 2:
+		cout << num1<<"-"<<num2<<"="<<num1-num2;					break;
+	case 3:
+		cout << num1<<"*"<<num2<<"="<<num1*num2;
+		break;
+	case 4:
+		cout << num1<<"/"<<num2<<"="<<num1/num2;
+		break;
+	default:
+		cout <<"error you did not select any of those option try agian later";
+		break;
+	}
+return 0;
+}
