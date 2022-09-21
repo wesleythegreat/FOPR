c61fa59eac88e580fce326595a6879ecdebea47e
wesleythegreat <gameking232nd@gmail.com>
2022-09-21 20:38:04 +0800

    fixed input and output system

--- 1stactivty.cpp
+++ 1stactivty.cpp
@@ -7,20 +7,23 @@ int main(){
 	cout <<"select a number for a type of operator will be used" << endl;
 	cout <<"1. addition"<<endl;
 	cout <<"2. subtraction"<<endl;
-	cout <<"3. division"<<endl;
+	cout <<"3. multiplication"<<endl;
+	cout <<"4. division"<<endl;
+	cin >> choice;
+	
 
 	switch(choice){
 	case 1:
-		cout << num1<<"+"<<num2<<"="<<num1+num2;
+		cout << num1<<"+"<<num2<<"= "<<num1+num2;
 		break;
 	case 2:
-		cout << num1<<"-"<<num2<<"="<<num1-num2;					
+		cout << num1<<"-"<<num2<<"= "<<num1-num2;					
 		break;
 	case 3:
-		cout << num1<<"*"<<num2<<"="<<num1*num2;
+		cout << num1<<"*"<<num2<<"= "<<num1*num2;
 		break;
 	case 4:
-		cout << num1<<"/"<<num2<<"="<<num1/num2;
+		cout << num1<<"/"<<num2<<"= "<<num1/num2;
 		break;
 	default:
 		cout <<"error you did not select any of those option try agian later";
